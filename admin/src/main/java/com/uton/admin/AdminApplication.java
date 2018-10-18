0cad9d38c56dd8408935002195577fcdb05d490c
runxinfu <furxswing@163.com>
2018-10-18 09:24:53 +0800

    删除不需要的class

--- admin/src/main/java/com/uton/admin/AdminApplication.java
+++ admin/src/main/java/com/uton/admin/AdminApplication.java
@@ -2,11 +2,18 @@ package com.uton.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdminApplication {
+public class AdminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AdminApplication.class);
     }
 }
